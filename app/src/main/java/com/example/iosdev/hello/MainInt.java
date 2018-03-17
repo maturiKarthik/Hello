ceed3f7b1702ea1399a86ea1d8807bb0c85d59da
iosdev <iosdev@PinkLady.local>
2018-03-18 00:31:20 +0100

    Initial commit hello world

---
+++ app/src/main/java/com/example/iosdev/hello/MainInt.java
@@ -0,0 +1,17 @@
+package com.example.iosdev.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by iosdev on 16/03/2018.
+ */
+
+public class MainInt extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_int);
+    }
+}
